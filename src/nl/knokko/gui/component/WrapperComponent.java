bb8c78e2b9838f49b0d048bdfa22229614b6a5b1
knokko <knokogator@hotmail.com>
2018-09-25 15:59:19 +0200

    Add wrapper component

---
+++ src/nl/knokko/gui/component/WrapperComponent.java
@@ -0,0 +1,106 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2018 20182191.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package nl.knokko.gui.component;
+
+import nl.knokko.gui.render.GuiRenderer;
+
+public class WrapperComponent extends AbstractGuiComponent {
+    
+    protected GuiComponent component;
+    
+    public WrapperComponent(GuiComponent component){
+        this.component = component;
+    }
+    
+    public void setComponent(GuiComponent component){
+        this.component = component;
+        if(component != null){
+            component.setState(state);
+            component.init();
+        }
+    }
+    
+    public GuiComponent getComponent(){
+        return component;
+    }
+    
+    public boolean isActive(){
+        return true;
+    }
+
+    @Override
+    public void init() {
+        if(component != null && isActive())
+            component.init();
+    }
+
+    @Override
+    public void update() {
+        if(component != null && isActive())
+            component.init();
+    }
+
+    @Override
+    public void render(GuiRenderer renderer) {
+        if(component != null && isActive())
+            component.render(renderer);
+    }
+
+    @Override
+    public void click(float x, float y, int button) {
+        if(component != null && isActive())
+            component.click(x, y, button);
+    }
+
+    @Override
+    public void clickOut(int button) {
+        if(component != null && isActive())
+            component.clickOut(button);
+    }
+
+    @Override
+    public boolean scroll(float amount) {
+        if(component != null && isActive())
+            return component.scroll(amount);
+        return false;
+    }
+
+    @Override
+    public void keyPressed(int keyCode) {
+        if(component != null && isActive())
+            component.keyPressed(keyCode);
+    }
+
+    @Override
+    public void keyPressed(char character) {
+        if(component != null && isActive())
+            component.keyPressed(character);
+    }
+
+    @Override
+    public void keyReleased(int keyCode) {
+        if(component != null && isActive())
+            component.keyReleased(keyCode);
+    }
+}
