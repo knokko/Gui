a2b98a4c81b6a4ad078a8345b7a48b9df67c7b54
knokko <knokogator@hotmail.com>
2019-08-01 21:57:16 +0200

    Use FileSystemView instead of ShellFolder because ShellFolder requires the sun package, which is discouraged to use

--- src/nl/knokko/gui/component/menu/FileChooserMenu.java
+++ src/nl/knokko/gui/component/menu/FileChooserMenu.java
@@ -25,10 +25,11 @@ package nl.knokko.gui.component.menu;
 
 import java.awt.Color;
 import java.awt.Graphics2D;
-import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.io.File;
-import java.io.FileNotFoundException;
+
+import javax.swing.Icon;
+import javax.swing.filechooser.FileSystemView;
 
 import nl.knokko.gui.color.GuiColor;
 import nl.knokko.gui.color.SimpleGuiColor;
@@ -39,21 +40,24 @@ import nl.knokko.gui.component.text.ConditionalTextButton;
 import nl.knokko.gui.component.text.TextButton;
 import nl.knokko.gui.component.text.dynamic.DynamicTextButton;
 import nl.knokko.gui.util.TextBuilder.Properties;
-import sun.awt.shell.ShellFolder;
 
 public class FileChooserMenu extends GuiMenu {
-	
-	public static final Properties CANCEL_PROPERTIES = Properties.createButton(new Color(200, 200, 200), new Color(150, 150, 250));
-	public static final Properties CANCEL_HOVER_PROPERTIES = Properties.createButton(Color.WHITE, new Color(200, 200, 255));
-	
-	public static final Properties SELECT_PROPERTIES = Properties.createButton(new Color(150, 150, 200), new Color(120, 120, 250));
-	public static final Properties SELECT_HOVER_PROPERTIES = Properties.createButton(new Color(100, 100, 255), Color.BLUE);
-	
+
+	public static final Properties CANCEL_PROPERTIES = Properties.createButton(new Color(200, 200, 200),
+			new Color(150, 150, 250));
+	public static final Properties CANCEL_HOVER_PROPERTIES = Properties.createButton(Color.WHITE,
+			new Color(200, 200, 255));
+
+	public static final Properties SELECT_PROPERTIES = Properties.createButton(new Color(150, 150, 200),
+			new Color(120, 120, 250));
+	public static final Properties SELECT_HOVER_PROPERTIES = Properties.createButton(new Color(100, 100, 255),
+			Color.BLUE);
+
 	protected final FileListener listener;
 	protected final FileFilter filter;
 	protected final GuiComponent returnMenu;
 	protected FileList list;
-	
+
 	protected File selectedFile;
 	protected File directory;
 	protected File parentDirectory;
@@ -64,7 +68,7 @@ public class FileChooserMenu extends GuiMenu {
 		this.filter = filter;
 		this.directory = new File("").getAbsoluteFile();
 		this.parentDirectory = directory.getParentFile();
-		
+
 		// TODO Add a constructor for custom props and background color
 	}
 
@@ -89,75 +93,79 @@ public class FileChooserMenu extends GuiMenu {
 			return parentDirectory != null;
 		}), 0.25f, 0.88f, 0.35f, 0.98f);
 	}
-	
+
 	protected void setDirectory(File newDirectory) {
 		directory = newDirectory;
 		parentDirectory = directory.getParentFile();
 		list.setDirectory();
 		state.getWindow().markChange();
 	}
-	
+
 	private static final GuiColor LIST_BACKGROUND = new SimpleGuiColor(0, 0, 150);
-	
+
 	public static final Properties FILE_NAME_PROPERTIES = Properties.createLabel(Color.BLACK, Color.WHITE, 512, 128);
-	public static final Properties FILE_NAME_HOVER_PROPERTIES = Properties.createLabel(new Color(50, 50, 50), new Color(150, 150, 255), 512, 128);
+	public static final Properties FILE_NAME_HOVER_PROPERTIES = Properties.createLabel(new Color(50, 50, 50),
+			new Color(150, 150, 255), 512, 128);
 	public static final Properties FOLDER_NAME_PROPERTIES = Properties.createLabel(Color.BLACK, Color.WHITE, 512, 128);
-	public static final Properties FOLDER_NAME_HOVER_PROPERTIES = Properties.createLabel(new Color(50, 50, 50), new Color(150, 150, 255), 512, 128);
-	
+	public static final Properties FOLDER_NAME_HOVER_PROPERTIES = Properties.createLabel(new Color(50, 50, 50),
+			new Color(150, 150, 255), 512, 128);
+
 	protected class FileList extends GuiMenu {
 
 		@Override
 		protected void addComponents() {
 			setDirectory();
 		}
-		
+
 		@Override
 		public GuiColor getBackgroundColor() {
 			return LIST_BACKGROUND;
 		}
-		
+
 		protected void setDirectory() {
 			clearComponents();
 			File[] files = directory.listFiles();
 			int index = 0;
-			for(File file : files) {
-				try {
-					if(file.isDirectory() || filter.accept(file)) {
-						
-						// TODO Reuse buffered images containing icons
-						Image icon = ShellFolder.getShellFolder(file).getIcon(true);
-						BufferedImage image = new BufferedImage(icon.getWidth(null), icon.getHeight(null), BufferedImage.TYPE_INT_ARGB);
-						Graphics2D g = image.createGraphics();
-						g.drawImage(icon, 0, 0, null);
-						g.dispose();
-						addComponent(new SimpleImageComponent(state.getWindow().getTextureLoader().loadTexture(image)), 0f, 0.9f - index * 0.1f, 0.1f, 1f - index * 0.1f);
-						if(file.isDirectory()) {
-							addComponent(new DynamicTextButton(file.getName(), FILE_NAME_PROPERTIES, FILE_NAME_HOVER_PROPERTIES, () -> {
-								FileChooserMenu.this.setDirectory(file);
-							}), 0.15f, 0.9f - index * 0.1f, Math.min(1f, 0.15f + file.getName().length() * 0.02f), 1f - index * 0.1f);
-						} else {
-							addComponent(new DynamicTextButton(file.getName(), FOLDER_NAME_PROPERTIES, FOLDER_NAME_HOVER_PROPERTIES, () -> {
-								selectedFile = file;
-								state.getWindow().markChange();
-							}), 0.15f, 0.9f - index * 0.1f, Math.min(1f, 0.15f + file.getName().length() * 0.02f), 1f - index * 0.1f);
-						}
-						index++;
+			for (File file : files) {
+				if (file.isDirectory() || filter.accept(file)) {
+
+					// TODO Reuse buffered images containing icons
+					Icon icon = FileSystemView.getFileSystemView().getSystemIcon(file);
+					BufferedImage image = new BufferedImage(icon.getIconWidth(), icon.getIconHeight(),
+							BufferedImage.TYPE_INT_ARGB);
+					Graphics2D g = image.createGraphics();
+					icon.paintIcon(null, g, 0, 0);
+					g.dispose();
+					addComponent(new SimpleImageComponent(state.getWindow().getTextureLoader().loadTexture(image)), 0f,
+							0.9f - index * 0.1f, 0.1f, 1f - index * 0.1f);
+					if (file.isDirectory()) {
+						addComponent(new DynamicTextButton(file.getName(), FILE_NAME_PROPERTIES,
+								FILE_NAME_HOVER_PROPERTIES, () -> {
+									FileChooserMenu.this.setDirectory(file);
+								}), 0.15f, 0.9f - index * 0.1f, Math.min(1f, 0.15f + file.getName().length() * 0.02f),
+								1f - index * 0.1f);
+					} else {
+						addComponent(new DynamicTextButton(file.getName(), FOLDER_NAME_PROPERTIES,
+								FOLDER_NAME_HOVER_PROPERTIES, () -> {
+									selectedFile = file;
+									state.getWindow().markChange();
+								}), 0.15f, 0.9f - index * 0.1f, Math.min(1f, 0.15f + file.getName().length() * 0.02f),
+								1f - index * 0.1f);
 					}
-				} catch(FileNotFoundException fnfe) {
-					System.out.println("Couldn't 'find'" + file + ": " + fnfe.getMessage());
+					index++;
 				}
 			}
 			this.screenCenterY = 0f;
 		}
 	}
-	
+
 	public static interface FileListener {
-		
+
 		void onChoose(File file);
 	}
-	
+
 	public static interface FileFilter {
-		
+
 		boolean accept(File file);
 	}
 }
